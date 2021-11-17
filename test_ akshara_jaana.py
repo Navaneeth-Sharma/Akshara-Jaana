a9061238185e361ff869c5cbf1b2ae722f645377
Christian Clauss <cclauss@me.com>
2021-11-17 13:31:35 +0100

    Update test_ akshara_jaana.py

--- test_ akshara_jaana.py
+++ test_ akshara_jaana.py
@@ -1,6 +1,8 @@
-import AksharaJaana.main as ak 
-text = ak.ocr_engine('/home/navaneeth/Desktop/NandD/OCR_kannada/CamScanner 06-28-2020 12.12.10.pdf')
-
+import AksharaJaana.main as ak
 from AksharaJaana.utils import utils
-u = utils()
-u.write_as_RTF(text, saving_path='/home/navaneeth/Desktop/1.rtf')
+
+
+def test_write_as_RTF():
+    text = ak.ocr_engine('/home/navaneeth/Desktop/NandD/OCR_kannada/CamScanner 06-28-2020 12.12.10.pdf')
+    u = utils()
+    u.write_as_RTF(text, saving_path='/home/navaneeth/Desktop/1.rtf')
