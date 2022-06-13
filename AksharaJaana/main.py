6819c28813f10993b41f2d3961d4f0a18c52f59b
Navaneeth Sharma <63489382+Navaneeth-Sharma@users.noreply.github.com>
2022-06-13 19:19:18 +0530

    Update main.py

--- AksharaJaana/main.py
+++ AksharaJaana/main.py
@@ -1,4 +1,8 @@
-import os,shutil,glob
+import os
+import glob
+from os.path import exists
+from warnings import warn
+
 try:
 	from utils import utils
 except:
@@ -8,83 +12,87 @@ utils = utils()
 
 
 def ocr_engine(filename):
-  try:
-    os.mkdir("output/")
-  except:
-    pass
-  line_dir = "output/"
-
-  if ".pdf" in filename:
+  if exists(filename):
     try:
-      f1 = open(filename)
-      text = f1.read()
-      if text is []:
-        pass
-      else:
-        return text
-    except FileNotFoundError:
-        print("There is no file named", filename)
-      
-
-    try:
-      os.mkdir(line_dir + "pdfout/")
-    except:
+      os.mkdir("output/")
+    except FileExistsError:
       pass
-    files = glob.glob(line_dir + "pdfout/*")
-    for f in files:
-        os.remove(f)
-
-    try:
-      from pdf2image import pdfinfo_from_path, convert_from_path
-      info = pdfinfo_from_path(filename, userpw=None, poppler_path=None)
-
-      maxPages = info["Pages"]
-      for page in range(1, maxPages + 1, 20): 
-        pages = convert_from_path(filename, dpi=100, first_page=page, last_page=min(page + 20 - 1,maxPages))
-
-        for page1, i in zip(pages,range(len(pages))):
-          print(i)
-          page1.save(line_dir + "pdfout/out{0}.jpg".format(page+i), "JPEG")
-    except:
-      pass
-
-
     line_dir = "output/"
 
-    try:
-        os.mkdir(line_dir)
-    except:
+    if ".pdf" in filename:
+      try:
+        f1 = open(filename)
+        text = f1.read()
+        if text is []:
+          pass
+        else:
+          return text
+      except FileNotFoundError:
+          print("There is no file named", filename)
+        
+
+      try:
+        os.mkdir(line_dir + "pdfout/")
+      except:
+        pass
+      files = glob.glob(line_dir + "pdfout/*")
+      for f in files:
+          os.remove(f)
+
+      try:
+        from pdf2image import pdfinfo_from_path, convert_from_path
+        info = pdfinfo_from_path(filename, userpw=None, poppler_path=None)
+
+        maxPages = info["Pages"]
+        for page in range(1, maxPages + 1, 20): 
+          pages = convert_from_path(filename, dpi=100, first_page=page, last_page=min(page + 20 - 1,maxPages))
+
+          for page1, i in zip(pages,range(len(pages))):
+            print(i)
+            page1.save(line_dir + "pdfout/out{0}.jpg".format(page+i), "JPEG")
+      except:
         pass
 
-    files = glob.glob(line_dir + "*.png")
-           
-    for f in files:
-        try:
-            os.unlink(f)
-        except OSError as e:
-            print("Error: %s : %s" % (f, e.strerror))
 
-    utils.truncate_data()
+      line_dir = "output/"
 
-    files = glob.glob(line_dir + "pdfout/*")
-
-    list_of_line_img= os.listdir(line_dir + "pdfout/")
-
-    arr = utils.rearrange(list_of_line_img)
-
-    for f in arr:
       try:
-        text += utils.Ocr_image("output/pdfout/" + f)
-
+          os.mkdir(line_dir)
       except:
-        text = utils.Ocr_image("output/pdfout/" + f)
-    
-    try:
-      return text
-    except:
-      pass
+          pass
 
+      files = glob.glob(line_dir + "*.png")
+            
+      for f in files:
+          try:
+              os.unlink(f)
+          except OSError as e:
+              print("Error: %s : %s" % (f, e.strerror))
+
+      utils.truncate_data()
+
+      files = glob.glob(line_dir + "pdfout/*")
+
+      list_of_line_img= os.listdir(line_dir + "pdfout/")
+
+      arr = utils.rearrange(list_of_line_img)
+
+      for f in arr:
+        try:
+          text += utils.Ocr_image("output/pdfout/" + f)
+
+        except:
+          text = utils.Ocr_image("output/pdfout/" + f)
+      
+      try:
+        return text
+      except:
+        pass
+
+    else:
+      utils.truncate_data()
+      text = utils.Ocr_image(filename)
+      return text
+  
   else:
-    utils.truncate_data()
-    text = utils.Ocr_image(filename)
-    return text
+    warn("\033[93m The file in the given path doesn't exists. Please give valid path!")
