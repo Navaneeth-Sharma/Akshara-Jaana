05b9e70f93390475cdd422b354acea78bb80bba4
Navaneeth Sharma <63489382+Navaneeth-Sharma@users.noreply.github.com>
2023-11-08 19:18:16 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,14 +5,14 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="AksharaJaana",
-    version="1.0.1.2",
+    version="1.0.1.3",
     author="Navaneeth",
     author_email="navaneethsharma2310oct@gmail.com",
     description="A Kannada OCR",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/Navaneeth-Sharma/Akshara-Jaana/",
-    packages=setuptools.find_packages(),
+    packages=setuptools.find_packages(include=['AksharaJaana']),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
